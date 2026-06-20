33f9b68cd6ecb08af607d4d0ab2a7786b0e5591f
Lucas <lucaslls.1989@hotmail.com>
2026-06-20 11:47:51 -0300

    Corrige configuracao do deploy Vercel

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,7 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_adonai.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "config.settings"
+)
 
 application = get_wsgi_application()
 
