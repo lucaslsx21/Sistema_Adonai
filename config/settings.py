b916d79c3f937431a636f05ec8c0cf91ade33e73
Lucas <lucaslls.1989@hotmail.com>
2026-06-24 10:25:06 -0300

    Configuração do deploy

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0lmls4yy!l3#_3*vbnuw(ryeb!6=ikf+oum^r6w&dadn2glny!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
